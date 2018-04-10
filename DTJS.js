35b0061e1c39e68e04290d75e40567025f52dba9
CodeTiger <2624183401@qq.com>
2018-04-09 20:51:55 -0400

    Update DTJS.js
    add the KNN algorithm and organized anything a bit

--- DTJS.js
+++ DTJS.js
@@ -1,3 +1,102 @@
+//KNN SECTION START
+
+function KNN(data,label) {
+	this.data = data;
+	this.label = label;
+}
+
+KNN.prototype.normalize = function() {
+	var small = [];
+	var big = [];
+	for(var i = 0;i < this.data[0].length;i++) {
+		small[i] = Number.MAX_VALUE;
+		big[i] = Number.MIN_VALUE;
+	}
+	for(var i = 0;i < this.data.length;i++) {	
+		for(var j = 0;j < this.data[i].length;j++) {
+			if(this.data[i][j] < small[j]) {
+				small[j] = this.data[i][j];
+			}
+			if(this.data[i][j] > big[j]) {
+				big[j] = this.data[i][j];
+			}
+		}
+	}
+	for(var i = 0;i < this.data[0].length;i++) {
+		var dist = big[i] - small[i];
+		for(var j = 0;j < this.data.length;j++) {
+			this.data[j][i] = (this.data[j][i] - small[i]) / dist;
+		}
+	}
+}
+
+KNN.prototype.result = function(k,inp) {
+	if(k > this.data.length) {
+		console.log("Error: K is bigger than the number of datas");
+		return;
+	}
+	var dist = [];
+	for(var i = 0;i < this.data.length;i++) {
+		dist[i] = this.dist(inp,this.data[i]);
+	}
+
+	var result = new Array(k);
+	for(var i = 0;i < k;i++) {
+		var ans;
+		var min = Number.MAX_VALUE;
+		for(var j = 0;j < dist.length;j++) {
+			if(dist[j] < min) {
+				min = dist[j];
+				ans = j;
+			}else if(dist[j] == min) {
+				if(Math.random() >= 0.5) {
+					min = dist[j];
+					ans = j;
+				}
+			}
+		}
+		dist[ans] = Number.MAX_VALUE;
+		result[i] = ans;
+	}
+	var count = new Object();
+	var labels = [];
+	for(var i = 0;i < result.length;i++) {
+		if(count[this.label[result[i]]] == null) {
+			count[this.label[result[i]]] = 0;
+			labels.push(this.label[result[i]]);
+		}
+		count[this.label[result[i]]] = 0;
+	}
+	var ans = [];
+	var numb = 0;
+	for(var i = 0;i < labels.length;i++) {
+		if(count[labels[i]] > numb) {
+			ans = [];
+			numb = count[labels[i]];
+			ans.push(labels[i]);
+		}else if(count[labels[i]] == numb) {
+			ans.push(labels[i]);
+		}
+	}
+	var final = Math.floor(Math.random() * ans.length);
+	return ans[final];
+}
+
+KNN.prototype.dist = function(x,y) {
+	var total = 0;
+	for(var i = 0;i < x.length;i++) {
+		total += (y[i] - x[i]) * (y[i] - x[i]);
+	}
+	return Math.sqrt(total);
+}
+
+//KNN SECTION END
+
+
+
+
+//MLPNN SECTION START
+
 function MLPNN(inp,hid,out,data,label,lr,actfun) {
 	this.inp = inp;
 	this.hid = hid;
@@ -49,6 +148,8 @@ MLPNN.prototype.init = function(b) {
 
 MLPNN.prototype.activate = function(w,inp) {
 	var result = w[0];
+	console.log("Hello world");
+
 	for(var i = 1;i < w.length;i++) {
 		result += w[i] * inp[i - 1];
 	}
@@ -156,3 +257,5 @@ function tanh(n,b) {
 		return Math.tanh(n);
 	}
 }
+
+//MLPNN SECTION END
